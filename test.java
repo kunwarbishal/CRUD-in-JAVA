fa4c2e6920d151dc06479e8544b6a0bd57455b86
Bishal Kunwar <101706114+Bishallk@users.noreply.github.com>
2023-07-04 10:59:46 +0545

    added  feature 2

--- test.java
+++ test.java
@@ -1,15 +1,22 @@
 import java.*;
+
 public class test {
 
-    public  static void  test(){
+    public static void test() {
         System.out.println("This is test");
 
-      }
-      public  static void printName(String name){
-          System.out.println("your name :"+name);
+    }
 
-      }
-      public static void feature1(){
-          System.out.println("this is feature 1");
-      }
+    public static void printName(String name) {
+        System.out.println("your name :" + name);
+
+    }
+
+    public static void feature1() {
+        System.out.println("this is feature 1");
+    }
+
+    public static void feature2() {
+        System.out.println("this is feature 2");
+    }
 }
