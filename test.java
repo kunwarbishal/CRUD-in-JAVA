74a586aef51621f240cb2911e91fe13eab8fa62d
Bishal Kunwar <101706114+Bishallk@users.noreply.github.com>
2023-07-04 10:43:22 +0545

    added method feature 1

--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+import java.*;
 public class test {
 
     public  static void  test(){
@@ -7,5 +8,8 @@ public class test {
       public  static void printName(String name){
           System.out.println("your name :"+name);
 
+      }
+      public static void feature1(){
+          System.out.println("this is feature 1");
       }
 }
