e6bd57703185227331c41bd769c945a7b6312518
Bishal Kunwar <101706114+Bishallk@users.noreply.github.com>
2023-07-04 10:38:28 +0545

    added printName method

--- test.java
+++ test.java
@@ -1,6 +1,11 @@
 public class test {
+
     public  static void  test(){
         System.out.println("This is test");
+
+      }
+      public  static void printName(String name){
+          System.out.println("your name :"+name);
 
       }
 }
