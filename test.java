46ede77a5bcb6c3d038fd49ebe126949d479119c
Bishal Kunwar <85816979+kunwarbishal@users.noreply.github.com>
2023-07-04 13:42:31 +0545

    feature 4 (#1)
    * added  feature 2
    
    * added feature 3
    
    * featuere 4 added
    
    ---------
    
    Co-authored-by: Bishal Kunwar <101706114+Bishallk@users.noreply.github.com>

--- test.java
+++ test.java
@@ -1,15 +1,28 @@
 import java.*;
+
 public class test {
 
-    public  static void  test(){
+    public static void test() {
         System.out.println("This is test");
 
-      }
-      public  static void printName(String name){
-          System.out.println("your name :"+name);
+    }
 
-      }
-      public static void feature1(){
-          System.out.println("this is feature 1");
-      }
+    public static void printName(String name) {
+        System.out.println("your name :" + name);
+
+    }
+
+    public static void feature1() {
+        System.out.println("this is feature 1");
+    }
+
+    public static void feature2() {
+        System.out.println("this is feature 2");
+    }
+    public static void feature3() {
+        System.out.println("this is feature 3");
+    }
+    public static void feature4(){
+        System.out.println("this is feature 4");
+    }
 }
